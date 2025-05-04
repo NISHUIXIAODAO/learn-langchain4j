f9cbcb1520b9f081ccde50fcd65bfd37329c9d0b
NISHUIXIAODAO <1814823843@qq.com>
2025-05-04 19:30:18 +0800

    finish

--- src/main/java/com/may/java/ai/langchain4j/assistant/XiaomaAgent.java
+++ src/main/java/com/may/java/ai/langchain4j/assistant/XiaomaAgent.java
@@ -7,12 +7,21 @@ import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 import reactor.core.publisher.Flux;
 
+/***
+ * 配置Agent：
+ *  流式输出ChatModel
+ *  聊天记忆
+ *  自定义工具
+ *  向量存储Pinecone
+ *
+ *  系统提示词
+ */
 @AiService(
         wiringMode = AiServiceWiringMode.EXPLICIT,
         streamingChatModel = "qwenStreamingChatModel",
         chatMemoryProvider = "chatMemoryProviderXiaoma",
         tools = "appointmentTools",
-        contentRetriever = "contentRetrieverXiaomaPincone" //配置向量存储
+        contentRetriever = "contentRetrieverXiaomaPincone"
 )
 public interface XiaomaAgent {
     @SystemMessage(fromResource = "xiaoma-prompt-template.txt")
